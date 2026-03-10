491863575a4ad50d2d35fea851ff7a9b10eb2684
heakknight <heakmeng42@gmail.com>
2026-03-10 19:22:12 +0700

    Setup Spring project for homework

---
+++ src/main/java/com/phengmengheak/_3_pheng_mengheak_spring_homework001/Application.java
@@ -0,0 +1,13 @@
+package com.phengmengheak._3_pheng_mengheak_spring_homework001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/phengmengheak/_3_pheng_mengheak_spring_homework001/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phengmengheak._3_pheng_mengheak_spring_homework001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
